3bef5588c2c9d4b5f9b1aa25d667b0c92bbac0c9
luiz filipi <luizfilipi3246@gmail.com>
2025-04-28 11:34:14 -0300

    sidebar ta paiando

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import Sidebar from './components/sidebar'
+import About from './components/About'
 
 function App() {
   return (
@@ -7,6 +8,7 @@ function App() {
       <h1>Luiz Filipi</h1>
 
       <Sidebar />
+      <About />
     </div>
   )
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,11 @@
+const About = () => {
+    return(
+    <div>
+        <h1>Sobre</h1>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Perspiciatis amet doloribus odit molestias totam provident impedit, dolor eveniet officia numquam quibusdam ipsam placeat tempore quae facere asperiores quidem sequi aut!</p>
+        
+    </div>
+    ) 
+}
+
+export default About
